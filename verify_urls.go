7ca82b45d86b5d76a2100fe98d3f8addc1b4c7e5
Fabio Bauer <22272392+fabiobauer@users.noreply.github.com>
2021-10-22 20:48:25 +0200

    CS

--- verify_urls.go
+++ verify_urls.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"regexp"
-	"time"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "os"
+    "regexp"
+    "time"
 )
 
 func main() {
-	fmt.Println("Verifying URLs..")
+    fmt.Println("Verifying URLs..")
 
-	readmeFile, err := ioutil.ReadFile("README.md")
-	if err != nil {
-		fmt.Println("Could not find README!")
-		os.Exit(1)
-	}
+    readmeFile, err := ioutil.ReadFile("README.md")
+    if err != nil {
+        fmt.Println("Could not find README!")
+        os.Exit(1)
+    }
 
-	fileContent := string(readmeFile)
-	urlElementRegex := regexp.MustCompile(`(?m)\[.+?]\(((http|https)://.+?)\)`)
+    fileContent := string(readmeFile)
+    urlElementRegex := regexp.MustCompile(`(?m)\[.+?]\(((http|https)://.+?)\)`)
 
-	httpClient := http.Client{Timeout: 10 * time.Second}
+    httpClient := http.Client{Timeout: 10 * time.Second}
 
-	var brokenUrls []string
-	for _, urlElement := range urlElementRegex.FindAllStringSubmatch(fileContent, -1) {
-		var url = urlElement[1]
+    var brokenUrls []string
+    for _, urlElement := range urlElementRegex.FindAllStringSubmatch(fileContent, -1) {
+        var url = urlElement[1]
 
-		fmt.Printf("Checking %s: ", url)
+        fmt.Printf("Checking %s: ", url)
 
-		resp, err := httpClient.Get(url)
-		if err != nil || resp.StatusCode != 200 {
-			brokenUrls = append(brokenUrls, url)
-			fmt.Println("FAILED - ", err)
-		} else {
-			fmt.Println("OK")
-		}
-	}
+        resp, err := httpClient.Get(url)
+        if err != nil || resp.StatusCode != 200 {
+            brokenUrls = append(brokenUrls, url)
+            fmt.Println("FAILED - ", err)
+        } else {
+            fmt.Println("OK")
+        }
+    }
 
-	if len(brokenUrls) != 0 {
-		fmt.Println("Broken URLs were found:")
-		for _, brokenUrl := range brokenUrls {
-			fmt.Println(brokenUrl)
-		}
+    if len(brokenUrls) != 0 {
+        fmt.Println("Broken URLs were found:")
+        for _, brokenUrl := range brokenUrls {
+            fmt.Println(brokenUrl)
+        }
 
-		os.Exit(1)
-	}
+        os.Exit(1)
+    }
 
-	fmt.Println("No broken URLs found!")
-	os.Exit(0)
+    fmt.Println("No broken URLs found!")
+    os.Exit(0)
 }
