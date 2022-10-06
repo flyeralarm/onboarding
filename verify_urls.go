db5f980b99e55da2a495428490d986fdeeb4c2ac
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2022-10-06 13:46:57 +0200

    adjustments so the script works with all urls - replaced depreciated ioutil.ReadFile() with os.ReadFile() - added support for http/2 - added a user agent - if there is no errormessage, show the http status text

--- verify_urls.go
+++ verify_urls.go
@@ -1,52 +1,66 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
-    "regexp"
-    "time"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"regexp"
+	"time"
 )
 
 func main() {
-    fmt.Println("Verifying URLs..")
+	fmt.Println("Verifying URLs..")
 
-    readmeFile, err := ioutil.ReadFile("README.md")
-    if err != nil {
-        fmt.Println("Could not find README!")
-        os.Exit(1)
-    }
+	readmeFile, err := os.ReadFile("README.md")
+	if err != nil {
+		fmt.Println("Could not find README!")
+		os.Exit(1)
+	}
 
-    fileContent := string(readmeFile)
-    urlElementRegex := regexp.MustCompile(`(?m)\[.+?]\(((http|https)://.+?)\)`)
+	fileContent := string(readmeFile)
+	urlElementRegex := regexp.MustCompile(`(?m)\[.+?]\(((http|https)://.+?)\)`)
 
-    httpClient := http.Client{Timeout: 20 * time.Second}
+	httpClient := http.Client{
+		Timeout: 20 * time.Second,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{},
+		},
+	}
 
-    var brokenUrls []string
-    for _, urlElement := range urlElementRegex.FindAllStringSubmatch(fileContent, -1) {
-        var url = urlElement[1]
+	var brokenUrls []string
+	for _, urlElement := range urlElementRegex.FindAllStringSubmatch(fileContent, -1) {
+		var url = urlElement[1]
 
-        fmt.Printf("Checking %s: ", url)
+		fmt.Printf("Checking %s: ", url)
 
-        resp, err := httpClient.Get(url)
-        if err != nil || resp.StatusCode != 200 {
-            brokenUrls = append(brokenUrls, url)
-            fmt.Println("FAILED - ", err)
-        } else {
-            fmt.Println("OK")
-        }
-    }
+		req, err := http.NewRequest("GET", url, nil)
+		req.Header.Add("User-Agent", "URL status code verification for the Flyeralarm onboarding resources; https://github.com/flyeralarm/onboarding")
+		resp, err := httpClient.Do(req)
 
-    if len(brokenUrls) != 0 {
-        fmt.Println("Broken URLs were found:")
-        for _, brokenUrl := range brokenUrls {
-            fmt.Println(brokenUrl)
-        }
+		errormessage := err
+		if errormessage == nil {
+			errormessage = errors.New(http.StatusText(resp.StatusCode))
+		}
 
-        os.Exit(1)
-    }
+		if err != nil || resp.StatusCode != 200 {
+			brokenUrls = append(brokenUrls, url)
+			fmt.Println("FAILED - ", errormessage)
+		} else {
+			fmt.Println("OK")
+		}
+	}
 
-    fmt.Println("No broken URLs found!")
-    os.Exit(0)
+	if len(brokenUrls) != 0 {
+		fmt.Println("Broken URLs were found:")
+		for _, brokenUrl := range brokenUrls {
+			fmt.Println(brokenUrl)
+		}
+
+		os.Exit(1)
+	}
+
+	fmt.Println("No broken URLs found!")
+	os.Exit(0)
 }
