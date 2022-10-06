82966e7a0c30de3fb07dc688fcb2fc4d109bf28a
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2022-10-06 13:49:08 +0200

    fixed tabs

--- verify_urls.go
+++ verify_urls.go
@@ -1,66 +1,66 @@
 package main
 
 import (
-	"crypto/tls"
-	"errors"
-	"fmt"
-	"net/http"
-	"os"
-	"regexp"
-	"time"
+    "crypto/tls"
+    "errors"
+    "fmt"
+    "net/http"
+    "os"
+    "regexp"
+    "time"
 )
 
 func main() {
-	fmt.Println("Verifying URLs..")
+    fmt.Println("Verifying URLs..")
 
-	readmeFile, err := os.ReadFile("README.md")
-	if err != nil {
-		fmt.Println("Could not find README!")
-		os.Exit(1)
-	}
+    readmeFile, err := os.ReadFile("README.md")
+    if err != nil {
+        fmt.Println("Could not find README!")
+        os.Exit(1)
+    }
 
-	fileContent := string(readmeFile)
-	urlElementRegex := regexp.MustCompile(`(?m)\[.+?]\(((http|https)://.+?)\)`)
+    fileContent := string(readmeFile)
+    urlElementRegex := regexp.MustCompile(`(?m)\[.+?]\(((http|https)://.+?)\)`)
 
-	httpClient := http.Client{
-		Timeout: 20 * time.Second,
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{},
-		},
-	}
+    httpClient := http.Client{
+        Timeout: 20 * time.Second,
+        Transport: &http.Transport{
+            TLSClientConfig: &tls.Config{},
+        },
+    }
 
-	var brokenUrls []string
-	for _, urlElement := range urlElementRegex.FindAllStringSubmatch(fileContent, -1) {
-		var url = urlElement[1]
+    var brokenUrls []string
+    for _, urlElement := range urlElementRegex.FindAllStringSubmatch(fileContent, -1) {
+        var url = urlElement[1]
 
-		fmt.Printf("Checking %s: ", url)
+        fmt.Printf("Checking %s: ", url)
 
-		req, err := http.NewRequest("GET", url, nil)
-		req.Header.Add("User-Agent", "URL status code verification for the Flyeralarm onboarding resources; https://github.com/flyeralarm/onboarding")
-		resp, err := httpClient.Do(req)
+        req, err := http.NewRequest("GET", url, nil)
+        req.Header.Add("User-Agent", "URL status code verification for the Flyeralarm onboarding resources; https://github.com/flyeralarm/onboarding")
+        resp, err := httpClient.Do(req)
 
-		errormessage := err
-		if errormessage == nil {
-			errormessage = errors.New(http.StatusText(resp.StatusCode))
-		}
+        errormessage := err
+        if errormessage == nil {
+            errormessage = errors.New(http.StatusText(resp.StatusCode))
+        }
 
-		if err != nil || resp.StatusCode != 200 {
-			brokenUrls = append(brokenUrls, url)
-			fmt.Println("FAILED - ", errormessage)
-		} else {
-			fmt.Println("OK")
-		}
-	}
+        if err != nil || resp.StatusCode != 200 {
+            brokenUrls = append(brokenUrls, url)
+            fmt.Println("FAILED - ", errormessage)
+        } else {
+            fmt.Println("OK")
+        }
+    }
 
-	if len(brokenUrls) != 0 {
-		fmt.Println("Broken URLs were found:")
-		for _, brokenUrl := range brokenUrls {
-			fmt.Println(brokenUrl)
-		}
+    if len(brokenUrls) != 0 {
+        fmt.Println("Broken URLs were found:")
+        for _, brokenUrl := range brokenUrls {
+            fmt.Println(brokenUrl)
+        }
 
-		os.Exit(1)
-	}
+        os.Exit(1)
+    }
 
-	fmt.Println("No broken URLs found!")
-	os.Exit(0)
+    fmt.Println("No broken URLs found!")
+    os.Exit(0)
 }
